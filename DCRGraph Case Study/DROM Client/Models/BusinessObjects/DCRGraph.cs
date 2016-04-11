a30701b423290700e0f020aacb975d91ecec2db2
archigo <aspo-ja@hotmail.com>
2016-04-11 16:13:23 +0200

    update order works, and state has been added to the dcr logic

--- DCRGraph Case Study/DROM Client/Models/BusinessObjects/DCRGraph.cs
+++ DCRGraph Case Study/DROM Client/Models/BusinessObjects/DCRGraph.cs
@@ -9,5 +9,6 @@ namespace DROM_Client.Models.BusinessObjects
     {
         public int Id { get; set; }
         public List<Event> Events { get; set; }
+        public bool state { get; set; }
     }
 }
